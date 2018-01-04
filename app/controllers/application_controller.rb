b9ffbc2004767123288b133e6ba1e10a59e38c82
vudngo <vudngo@gmail.com>
2018-01-04 14:51:00 -0800

    Fixes RAILS-1V

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,5 @@ class ApplicationController < ActionController::Base
   def set_raven_context
     Raven.user_context( email: @@email_address) if @@email_address
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-    #binding.pry
   end
 end

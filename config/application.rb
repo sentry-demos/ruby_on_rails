7ece6f62c751b1f0047e42610e320522dddd1dd6
Richard Propst <richard.propst@sentry.io>
2020-11-18 17:06:39 -0800

    Switch DSN to generic

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,6 @@ module TestRails
 end
 
 Sentry.init do |config|
-  config.dsn = 'https://769540fdd5bd44a7ae33afdfb3016924@o87286.ingest.sentry.io/5520625'
+  config.dsn = 'https://c74d756a9db448228ee9aa26ffab404e@sentry.io/1445853'
 end
 
